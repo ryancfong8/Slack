49fd2c33a7a23cc5886476c2d17abfa60800ae08
Ryan Fong <ryan.fong@sbcglobal.net>
2020-06-16 23:27:45 -0700

    Update production for websockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://chathero.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://chathero.herokuapp.com', 'http://chathero.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
